21e7cdf07ffff286cf88decabb58a4b714e823fa
Jong Seong Lee <jsl@icadyptes.cs.pdx.edu>
2016-07-27 00:42:15 -0700

    url node assignment implemented

--- exercises/node/url.js
+++ exercises/node/url.js
@@ -2,6 +2,46 @@ var http = require('http'); // do not change this line
 var url = require('url'); // do not change this line
 var querystring = require('querystring'); // do not change this line
 
+var server = http.createServer(function(req, res) {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('you have accessed the root');
+  } else if (req.url.indexOf('/test/') === 0) {
+		res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+
+    res.write('you have accessed \"' + decodeURIComponent(req.url.substr(6)) + '\" within test');
+		//decodeURIComponent(req.url.substr(6));
+  } else if (req.url.indexOf('/attributes') === 0) {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		var query = url.parse(req.url, true).query;
+		
+		var table_before = "<tr><td>";
+		var table_middle = "</td><td>";
+		var table_after = "</td></tr>";
+		var content = "";
+
+		for (var property in query) {
+			content += table_before + property + table_middle + query[property] + table_after;
+		}
+
+		if (content !== "") {
+			res.write('<table border=\"1\"><tbody>' + content + "</tbody></table>");
+		} else {
+			res.write('<table border=\"1\"></table>');
+		}
+  }
+
+  res.end();
+});
+
+server.listen(process.env.PORT || 8080);
+
 // create a server just like in the first exercise
 // as shown in the examples, you are asked to respond to various requests in different ways
 
