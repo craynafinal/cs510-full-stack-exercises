4218bb32e7a80ba62ab81b48bc6dc1b8e7a8cd26
Jong Seong Lee <jsl@macaroni.cs.pdx.edu>
2016-07-28 00:05:57 -0700

    http.js implemented

--- exercises/node/http.js
+++ exercises/node/http.js
@@ -1,5 +1,51 @@
 var http = require('http'); // do not change this line
 
+var server = http.createServer(function(req, res) {
+  if (req.url === '/missing') {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.write('your princess is in another castle');
+		//res.status(404).send('your princess is in another castle');
+  } else if (req.url === '/redirect') {
+		res.writeHead(302, { 'Location': '/redirected' });
+  } else if (req.url === '/cache') {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain',
+			'Cache-Control': 'max-age=86400'
+		});	
+		res.write('cache this resource');
+  } else if (req.url === '/cookie') {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain',
+			'Set-Cookie': 'hello=world'
+		});
+		res.write('i gave you a cookie');
+	
+  } else if (req.url === '/check') {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+
+/*
+		var incoming_cookies = '';
+
+		if (req.headers.cookie) {
+			incoming_cookies = cookie.parse(req.headers.cookie);
+		}	
+		*/
+		var incoming_cookies = req.headers.cookie;
+
+		if (incoming_cookies !== undefined && incoming_cookies.includes('hello')) {
+			res.write('yes');
+		} else {
+			res.write('no');
+		}
+  }
+
+  res.end();
+});
+
+server.listen(process.env.PORT || 8080);
+
 // create a server just like in the first exercise
 // as shown in the examples, you are asked to respond to various requests in different ways
 
