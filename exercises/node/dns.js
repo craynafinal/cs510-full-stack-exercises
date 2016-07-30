9e2317df8ee22fba6b0a94a973415d488ec9d3f6
Jong Seong Lee <jsl@pdx.edu>
2016-07-29 20:43:26 -0700

    node.js assignment completed

--- exercises/node/dns.js
+++ exercises/node/dns.js
@@ -1,6 +1,44 @@
 var http = require('http'); // do not change this line
 var dns = require('dns'); // do not change this line
 
+var server = http.createServer(function (req, res) {
+  // made a series of functions to avoid callback problem
+	var getIPAddress = function(urlAddress) {
+
+    dns.resolve4(urlAddress, function (err, addresses) {
+      if (err) {
+        errorCase();
+      } else {
+        successCase(addresses);
+      }
+    });
+  };
+
+  var errorCase = function() {
+    sendMessage('error');
+  };
+
+  var successCase = function(addresses) {
+    console.log('addresses: ' + JSON.stringify(addresses));
+    sendMessage(addresses.join('\n'));
+  };
+
+  var sendMessage = function(message) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+
+    res.write(message);
+    res.end();
+  };
+
+  getIPAddress(req.url.substring(1));
+});
+
+server.listen(process.env.PORT || 8080);
+
+// use dns.resolve4
+
 // create a server just like in the first exercise
 // as shown in the examples, you are asked to respond to various requests in different ways
 // this is an example of using an external resource
