6e4af35a7e8dab11ec64e807fc66a769410237f2
Jong Seong Lee <jsl@pdx.edu>
2016-07-29 23:20:01 -0700

    auth.js temp save

--- exercises/express/auth.js
+++ exercises/express/auth.js
@@ -4,6 +4,47 @@ var express = require('express'); // do not change this line
 var passport = require('passport'); // do not change this line
 var strategy = require('passport-http'); // do not change this line
 
+var BasicStrategy = strategy.BasicStrategy;
+var server = express();
+
+//server.use(passport.initialization());
+
+server.use(function(req, res, next) {
+  console.log('incoming request');
+  console.log(req.originalUrl);
+  next();
+});
+
+passport.use(new BasicStrategy(
+	function(username, password, done) {
+    if (username.valueOf() === 'test' &&
+      password.valueOf() === 'logmein')
+      return done(null, true);
+    else
+      return done(null, false);
+  }
+));
+
+server.get('/hello', function(req, res) {
+  res.status(200);
+
+  res.set({
+    'Content-Type': 'text/plain'
+  });
+	res.send('accessible to everyone');
+});
+
+server.get('/*', passport.authenticate('basic', { session: false }), function(req, res) {
+	res.status(200);
+
+	res.set({
+    'Content-Type': 'text/plain'
+  });
+  res.send('only accessible when logged in');
+
+});
+
+server.listen(process.env.PORT || 8080);
 // create an express server just like in the first exercise
 // as shown in the examples, you are asked to implement authentication check
 // the authentication check will only grant the user "test" with the password "logmein" acces
